03ac76172d83f2f44f38e4cbed6657afc1e7e7a8
ReynardStas <82842007+ReynardStas@users.noreply.github.com>
2022-08-24 21:21:14 +0300

    First commit
    Desktop and mobile version.

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Carousel
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+ const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 20,
+    startPosition: 0,
+    items: 1,
+    responsive : {
+        540 : {
+            items: 3,
+            startPosition: 1,   
+        },
+                // breakpoint from 1200 up
+        1200 : {
+            margin : 30,
+            items: 3,
+            startPosition: 1,
+        }
+    }
+});
+
+$('.slider__btn--prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+})
+
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('nav--mobile')
+    menuIcon.classList.toggle('menu-icon--active');
+    document.body.classList.toggle('no-scroll')
+}
